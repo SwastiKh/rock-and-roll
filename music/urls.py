7fa1857fc132ead4f88836b6c31c5c8e78388ec8
SwastiKh <swastikhurana0800@gmail.com>
2020-05-23 18:47:13 +0530

    view modified

--- music/urls.py
+++ music/urls.py
@@ -5,6 +5,10 @@ from django.conf import settings
 app_name="Music"
 
 urlpatterns = [
+    # /music/
     path('', views.index, name='index'),
     
+    #/music/71/
+    # url(r'^?P<album_id>[0-9]+)/$', views.detail, name='detail'')
+    path('music/<int:album_id>/', views.detail, name='detail')
 ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,29 @@
+from django.http import Http404 
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+#from django.template import loader
+from .models import Album
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>This is the music app homepage</h1>")
+    all_albums = Album.objects.all()
+    #template = loader.get_template('music/index.html')
+    #context = {'all_albums' : all_albums}
+    return render(request, 'music/index.html', {'all_albums' : all_albums})
+
+    #html = ''
+    #for album in all_albums:
+    #    path = 'music/' + str(album_id) + '/'
+    #    html += '<a href="' + path + '">' + album.album_title + '</a><br>'
+    #return HttpResponse(html)
+
+
+def detail(request, album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does nto exist")
+    return render(request, 'music/detail.html', {'album' : album})
+    
+    #return HttpResponse("<h2>details for Album id:" + str(album_id) + "</h2>")
