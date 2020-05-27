4c465e85a5a7485f46caad1690c5eb7eaaf658b9
SwastiKh <swastikhurana0800@gmail.com>
2020-05-27 12:09:39 +0530

    update and delete view done without frontend ; prob with manage.py(unrectified)

--- music/urls.py
+++ music/urls.py
@@ -12,6 +12,12 @@ urlpatterns = [
     # url(r'^?P<album_id>[0-9]+)/$', views.detail, name='detail'')
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 
-    # /music/album/add/
+    #/music/album/add/
     path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+
+    #/music/album/2/
+    path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album-update'),
+
+    #/music/album/2/delete
+    path('album/<int:pk>/delete/', views.AlbumDelete.as_view(), name='album-delete'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Album
 
 class IndexView(generic.ListView):
@@ -16,3 +17,11 @@ class DetailView(generic.DetailView):
 class AlbumCreate(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo']
+    
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
